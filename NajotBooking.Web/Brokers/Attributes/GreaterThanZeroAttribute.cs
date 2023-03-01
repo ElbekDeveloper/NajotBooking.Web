f21a16dfa714a43023b0c7616e30d8ea00f35c0e
khatanboyevelbek <programmerelbek@gmail.com>
2023-03-02 01:55:32 +0500

    BROKERS: Seat Validation Custom Attribute

---
+++ NajotBooking.Web/Brokers/Attributes/GreaterThanZeroAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NajotBooking.Web.Brokers.Attributes
+{
+    public class GreaterThanZeroAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object value, ValidationContext validationContext)
+        {
+            if(value is int intValue && intValue > 0)
+            {
+                return ValidationResult.Success;
+            }
+
+            else if (value is decimal decimalValue && decimalValue > 0)
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+               return new ValidationResult(ErrorMessage);
+            }
+        }
+    }
+}
