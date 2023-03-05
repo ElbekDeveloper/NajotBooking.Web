6ce1381ff744bd2ab99a226245327fa0fb412b74
khatanboyevelbek <programmerelbek@gmail.com>
2023-03-05 23:23:37 +0500

    PAGES: Delete Seat

---
+++ NajotBooking.Web/Pages/Seats/DeleteSeat.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NajotBooking.Web.Pages.Seats
+{
+    public partial class DeleteSeat
+    {
+        [Parameter]
+        public string SeatId { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var response = 
+                await httpClient.DeleteAsync($"https://najot-booking.azurewebsites.net/api/Seats/{SeatId}");
+
+            navigationManager.NavigateTo("/seats");
+        }
+    }
+}
