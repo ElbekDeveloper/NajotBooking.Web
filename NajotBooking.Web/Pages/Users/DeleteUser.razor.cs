29e290043dd0607ebeaddd54c6c57c84731a0254
Muhammad Muso <musogulomov123@gmail.com>
2023-03-08 14:04:40 +0500

    CODE RUB: Fix Delete Errors

---
+++ NajotBooking.Web/Pages/Users/DeleteUser.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using NajotBooking.Web.Services.UserServices;
+
+namespace NajotBooking.Web.Pages.Users
+{
+    public partial class DeleteUser
+    {
+        [Inject]
+        public IUserService UserService { get; set; }
+
+        [Inject]
+        public NavigationManager navManager { get; set; }
+
+        [Parameter]
+        public string userId { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await UserService.DeleteUserById(Guid.Parse(userId));
+            navManager.NavigateTo("/AllUsers");
+        }
+    }
+}
